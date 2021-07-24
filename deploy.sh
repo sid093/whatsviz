3e6a52295069ad5fb4c0182f28835971dd501931
Siddharth <siddharth.srivastava@sap.com>
2021-07-24 21:51:44 +0530

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+# Check directories needed
+mkdir -p ~/app/whatsviz/data/input ~/app/whatsviz/data/output
+
+# Pull latest
+docker pull sid093/whatsviz
+
+# Stop existing containers
+docker stop whatsviz
+docker rm whatsviz
+
+# Start container in background
+docker run -p 8000:8000 -v ~/app/whatsviz/data:/home/app/data -d --restart unless-stopped --name whatsviz sid093/whatsviz
