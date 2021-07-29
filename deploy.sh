6048a2cb0e411345feee034c4aaff5b197df9b56
Siddharth <siddharth.srivastava@sap.com>
2021-07-29 16:42:11 +0530

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,15 @@
-# Check directories needed
-mkdir -p ~/app/whatsviz/data/input ~/app/whatsviz/data/output
-rm -r ~/app/whatsviz/data/input/*
+WHATSVIZ_APP_HOME=~/app/whatsviz
+WHATSVIZ_GIT_REPO=https://github.com/sid093/whatsviz
 
 # Pull latest
-git -C repo pull || git clone https://github.com/sid093/whatsviz ~/app/whatsviz
-cd ~/app/whatsviz
+cd ~
+rm -rf $WHATSVIZ_APP_HOME
+git clone $WHATSVIZ_GIT_REPO $WHATSVIZ_APP_HOME
+cd $WHATSVIZ_APP_HOME
+
+# Check directories needed
+mkdir -p $WHATSVIZ_APP_HOME/data/input $WHATSVIZ_APP_HOME/data/output
+rm -r $WHATSVIZ_APP_HOME/data/input/*
 
 # Docker build
 docker build . -t whatsviz
@@ -13,4 +18,4 @@ docker build . -t whatsviz
 docker rm -f whatsviz
 
 # Start container in background
-docker run -p 8000:8000 -v ~/app/whatsviz/data:/home/app/data -d --restart unless-stopped --name whatsviz whatsviz
+docker run -p 8000:8000 -v $WHATSVIZ_APP_HOME/data:/home/app/data -d --restart unless-stopped --name whatsviz whatsviz
